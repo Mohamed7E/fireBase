76c2b5ff5ecadbaa2cc780b576424d8b1fa21a3b
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2022-12-21 19:22:20 +0200

    add

--- lib/Screens/Welcome_screen.dart
+++ lib/Screens/Welcome_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class WelcomScreen extends StatefulWidget {
   const WelcomScreen({super.key});
@@ -12,31 +12,42 @@ class WelcomScreen extends StatefulWidget {
 class _WelcomScreenState extends State<WelcomScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SafeArea(
-          child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/images/image1.png",
-                height: 300,
-              ),
-              const SizedBox(height: 20, ),
-              const Text("Let's get startrd " ,style: TextStyle(fontSize: 20,
-              fontWeight: FontWeight.bold,
-              ),
-              )
-            ]
-                
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/image1.png",
+                  height: 300,
                 ),
-              ),
-            
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Let's get startrd ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                const Text(
+                  "Naver a better time than now to start. ",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      
+      ),
     );
   }
 }
