b50540d1e57424377eda673c520eaa510bc0d18f
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2022-12-21 19:13:19 +0200

    add imge

--- lib/Screens/Welcome_screen.dart
+++ lib/Screens/Welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import'package:flutter/material.dart';
 
 class WelcomScreen extends StatefulWidget {
@@ -10,6 +12,31 @@ class WelcomScreen extends StatefulWidget {
 class _WelcomScreenState extends State<WelcomScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return  Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/image1.png",
+                height: 300,
+              ),
+              const SizedBox(height: 20, ),
+              const Text("Let's get startrd " ,style: TextStyle(fontSize: 20,
+              fontWeight: FontWeight.bold,
+              ),
+              )
+            ]
+                
+                ),
+              ),
+            
+          ),
+        ),
+      
+    );
   }
 }
