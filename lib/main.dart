4d41fe11bd8d2b8ea94d6b15d852d7892b713e33
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2022-12-21 18:45:43 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: WelcomeScreen(),
-      ),
-     
-    
-  }
+      );
+      }
 }
 
