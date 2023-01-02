203a0b7df7d71ac7daffbd5171129307b39f14ee
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2023-01-03 00:59:33 +0200

    homescreen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/Screens/Welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,8 +17,42 @@ class _HomeScreenState extends State<HomeScreen> {
     final ap = Provider.of<AuthProvider>(context, listen: false);
 
     return Scaffold(
-      body: Center(child: Text("00000000000")),
-      
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text("FlutterPhone Auth"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ap.userSignOut().then(
+                    (value) => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WelcomScreen(),
+                      ),
+                    ),
+                  );
+            },
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.purple,
+            backgroundImage: NetworkImage(ap.userModel.profilePic),
+            radius: 50,
+          ),
+          const SizedBox(height: 20),
+          Text(ap.userModel.name),
+          Text(ap.userModel.phoneNumber),
+          Text(ap.userModel.email),
+          Text(ap.userModel.bio),
+        ],
+      )),
     );
   }
 }
