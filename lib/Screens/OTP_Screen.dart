2d951cc6fbebe651ef9c9b342a40da5bc919a9a8
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2023-01-03 00:00:31 +0200

    UserInformationScreen

--- lib/Screens/OTP_Screen.dart
+++ lib/Screens/OTP_Screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/Screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
---
+++ lib/Screens/User_Information_Screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class UserInformationScreen extends StatefulWidget {
+  const UserInformationScreen({super.key});
+
+  @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
